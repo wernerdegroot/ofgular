0380efe39723b35f3772c1602348755daaef2095
wernerg <werner.degroot@ortec-finance.com>
2016-04-07 11:09:05 +0200

    Example: update an employee using a Focus

--- src/example/model/company/CompanyActionHandler.ts
+++ src/example/model/company/CompanyActionHandler.ts
@@ -2,6 +2,9 @@ import { Company } from 'example/model/company/Company';
 import { Person } from 'example/model/person/Person';
 import { PersonActionHandler } from 'example/model/person/PersonActionHandler';
 import { CompanyAction, EmployeeAction } from 'example/model/company/CompanyAction';
+import { Focus } from 'focus/Focus';
+import { Focussed } from 'focus/Focussed';
+import { createFocus, createListElementFocus } from 'focus/util';
 
 export class CompanyActionHandler {
 
@@ -9,27 +12,37 @@ export class CompanyActionHandler {
     public static $inject: string[] = [
         PersonActionHandler.className
     ];
-
+    
     public constructor(private personActionHandler: PersonActionHandler) {
 
     }
+    
+    private getEmployeesFocus(): Focus<Company, Person[]> {
+        return createFocus<Company, Person[]>(
+            company => company.employees, 
+            (company, employees) => new Company(company.name, company.employees));
+    }
+    
+    private getPersonWithIdFocus(id: number): Focus<Company, Person> {
+        
+        const personInPersonListFocus = createListElementFocus((person: Person) => person.id === id);
+        const employeesFocus = this.getEmployeesFocus(); 
+        
+        return employeesFocus.compose(personInPersonListFocus);
+    }
+    
+    private getFocussedOnPerson(id: number, company: Company): Focussed<Company, Person> {
+        return new Focussed<Company, Person>(this.getPersonWithIdFocus(id), company);
+    }
 
-    handle(model: Company, action: CompanyAction): Company {
+    public handle(model: Company, action: CompanyAction): Company {
 
         if (action instanceof EmployeeAction) {
 
-            const updatedEmployees: Person[]
-                = model.employees.map(employee => {
-
-                    if (employee.id === action.employeeId) {
-                        return this.personActionHandler.handle(employee, action.employeeAction);
-                    } else {
-                        return employee;
-                    }
-
-                });
-
-            return new Company(model.name, updatedEmployees);
+            return this
+                .getFocussedOnPerson(action.employeeId, model)
+                .updateValueWith(employee => this.personActionHandler.handle(employee, action.employeeAction))
+                .getRoot();
 
         }
 
--- src/focus/util.ts
+++ src/focus/util.ts
@@ -7,3 +7,28 @@ export function createFocus<BIG_TYPE, SMALL_TYPE>(
 		
 	return new ConcreteFocus(getter, setter);
 }
+
+export function createListElementFocus<T>(selector: (_0: T) => boolean): Focus<T[], T> {
+    
+    function getter(values: T[]): T {
+        const selectedValues = values.filter(selector);
+        return selectedValues.length > 0
+            ? selectedValues[0]
+            : null;
+    }
+    
+    function setter(values: T[], toSet: T): T[] {
+        let hasBeenSet = false;
+        return values.map(value => {
+            if (selector(value) && !hasBeenSet) {
+                hasBeenSet = true;
+                return toSet;
+            } else {
+                return value;
+            }
+        });
+    }
+    
+    return createFocus(getter, setter);
+    
+}
